008e7d211b891c27cfd8a389dc24441d82d2b083
felipscarvalho <felipecarvalho5520@gmail.com>
2024-10-14 14:41:09 -0300

    Added player guess function

--- scripts.js
+++ scripts.js
@@ -1,40 +1,64 @@
 const movies = [
-	{
-		title: "Dirty Dancing - Ritmo Quente",
-		acceptable_guess: [
-			"Dirty Dancing - Ritmo Quente",
-			"Dirty Dancing",
-			"Dirty Dancing Ritmo Quente",
-			"Dirty Dancing: Ritmo Quente",
-		],
-		start_image: "img_1",
-	},
-	{
-		title: "Thor: Ragnarok",
-		acceptable_guess: ["Thor: Ragnarok", "Thor Ragnarok", "Thor 3"],
-		start_image: "img_7",
-	},
-]
+  {
+    title: "Dirty Dancing - Ritmo Quente",
+    acceptable_guess: ["dirty dancing - ditmo duente", "dirty dancing", "dirty dancing ritmo quente", "dirty dancing: ritmo quente"],
+    start_image: "img_1",
+  },
+  {
+    title: "Thor: Ragnarok",
+    acceptable_guess: ["thor: ragnarok", "thor ragnarok", "thor 3"],
+    start_image: "img_7",
+  },
+];
 
 const getRandomGame = () => {
-	const randomIndex = Math.floor(Math.random() * movies.length)
+  const randomIndex = Math.floor(Math.random() * movies.length);
 
-	return movies[randomIndex]
-}
+  return movies[randomIndex];
+};
+
+const movie = getRandomGame();
 
 const startGame = () => {
-	document.getElementById("start-btn").style.display = "none"
+  document.getElementById("start-btn").style.display = "none";
 
-	const movie = getRandomGame()
-
-	const startCard = `
+  const startCard = `
 		<h2>Acerte o Filme</h2>
-		<img src="${`./assets/movies/${movie.start_image}.jpeg`}" alt="${`image_${movie.start_image.slice(-1)}`}" />
-		<form action="">
-			<input type="text" />
-			<button>Tentar</button>
+        <div id="movie-images">
+		    <img src="${`./assets/movies/${movie.start_image}.jpeg`}" alt="${`image_${movie.start_image.slice(-1)}`}" />
+        </div>
+		<form>
+			<input id="guess-input" type="text" />
+			<button type="button" onClick="guessMovie()">Tentar</button>
 		</form>
-    `
+    `;
 
-	document.getElementById("movie-card").innerHTML = startCard
-}
+  document.getElementById("movie-card").innerHTML = startCard;
+};
+
+const guessMovie = () => {
+  const playerGuess = document.getElementById("guess-input").value;
+
+  if (movie.acceptable_guess.includes(playerGuess.toLowerCase())) {
+    const movieCard = document.getElementById("movie-card");
+
+    movieCard.innerHTML = "<h2>Você acertou!!!</h2>" + movieCard.innerHTML;
+  } else {
+    const movieImages = document.getElementById("movie-images");
+    const imagesNumber = movieImages.childElementCount;
+
+    if (imagesNumber < 6) {
+      const firstImageNumber = movieImages.children[0].src.slice(-6, -5);
+      const newImageContainer = Object.freeze(document.createElement("div"));
+      newImageContainer.innerHTML = `<img src="${`./assets/movies/img_${+firstImageNumber + imagesNumber}.jpeg`}" alt="${`image_${
+        +firstImageNumber + imagesNumber
+      }`}" />`;
+
+      movieImages.appendChild(newImageContainer.firstChild);
+    } else {
+      const movieCard = document.getElementById("movie-card");
+
+      movieCard.innerHTML = "<h2>Você Falhou!!!</h2>" + movieCard.innerHTML;
+    }
+  }
+};
